9a032baae44272842d59b9a18c853ba0016c7b7a
anna_b <nelson_anna9@hotmail.com>
2024-02-26 14:45:53 -0600

    Reveal random image from api with button click

--- js/script.js
+++ js/script.js
@@ -6,7 +6,29 @@ const img = document.querySelector(".img");
 const getImage = async function () {
     const results = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await results.json();
-    console.log(images);
+    // console.log(images);
+    selectRandomImage(images);
 };
 
-getImage();
+
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    // console.log(randomImage);
+    displayImage(randomImage);
+};
+
+const displayImage = function (randomImage) {
+    const author = randomImage.author; 
+    const imgAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imgAddress;
+    imgDiv.classList.remove("hide");
+};
+
+button.addEventListener("click", function() {
+    getImage();
+});
+
+
+
